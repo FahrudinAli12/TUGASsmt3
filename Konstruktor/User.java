185e8eefe8613770f41e0c892d859a12d0a0a1ba
FahrudinAli12 <162128411+FahrudinAli12@users.noreply.github.com>
2025-01-15 11:21:05 +0700

    Add files via upload

---
+++ Konstruktor/User.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Konstruktor;
+
+/**
+ *
+ * @author ASUS TUF ROG
+ */
+public class User {
+    private String username;
+    private String password;
+    
+    public User(){
+        System.out.println("eksekusi method constructor...");
+    }
+    
+}
+
+class DemoConstructor{
+    public static void main(String[] args){
+        User petani = new User();
+    }
+}
+
