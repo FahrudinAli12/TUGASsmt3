cf683d18ad1a61927c9c9f6ac6464da878debf7c
FahrudinAli12 <162128411+FahrudinAli12@users.noreply.github.com>
2025-01-15 11:23:21 +0700

    Add files via upload

---
+++ OverideInheritance/Main.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package OverideInheritance;
+
+/**
+ *
+ * @author ASUS TUF ROG
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+        MobilMatic matic = new MobilMatic ();
+        matic.jalankan();
+    }
+}
---
+++ OverideInheritance/MobilMatic.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package OverideInheritance;
+
+/**
+ *
+ * @author ASUS TUF ROG
+ */
+public class MobilMatic extends Mobil {
+    @Override
+    public void jalankan(){
+        System.out.println("gigi di posisi D, tekan gas....");
+    }
+}
