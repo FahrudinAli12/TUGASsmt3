35ee2e1d797e960a29604d2b71aee10a6cadaa45
FahrudinAli12 <162128411+FahrudinAli12@users.noreply.github.com>
2025-01-15 11:23:54 +0700

    Add files via upload

---
+++ Pewarisan/Burung.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Pewarisan;
+
+/**
+ *
+ * @author ASUS TUF ROG
+ */
+ class Burung extends Enemy {
+  void walk(){
+      System.out.println("Burung Berjalan");
+  }
+  void jump(){
+      System.out.println("Burung Loncat-Loncat");
+  }
+  void Fly(){
+      System.out.println("Burung Terbang....");
+  }  
+}
---
+++ Pewarisan/Enemy.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Pewarisan;
+
+/**
+ *
+ * @author ASUS TUF ROG
+ */
+class Enemy {
+     String name = "fahru";
+     int Hp = 23;
+     int attackPoin = 85;
+     
+     void attack(){
+         System.out.println("Serang!");
+     }
+    
+}
