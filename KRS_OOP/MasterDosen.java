cdb07fc3bbebfe15683332d93c77ef71a1d74012
FahrudinAli12 <162128411+FahrudinAli12@users.noreply.github.com>
2025-01-15 11:21:24 +0700

    Add files via upload

---
+++ KRS_OOP/MasterDosen.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package KRS_OOP;
+
+/**
+ *
+ * @author ASUS TUF ROG
+ */
+public class MasterDosen {
+   String dosen1 = "redhitya wempi ansori";
+   String dosen2 = "winda khoirun nisak";
+   String dosen3 = "Abd Charis Fauzan";
+   String dosen4 = "muhamat maariful huda";
+   String dosen5 = "harliana";
+   String dosen6 = "tito prabowo";
+   String dosen7 = "drs imam machfud";
+}
